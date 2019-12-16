9f7ddbbadb789a68d965c97b9c89a2ca25b98b9a
Alba Gómez Pérez <albagope@gmail.com>
2019-12-16 16:02:55 +0100

    do petition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import {GetEpisodes} from './services/GetEpisodes';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      AllCharacters: [],
+    };
+  }
+
+
+
+// Función sirve para pintar la petición al entrar en la página
+  componentDidMount() {
+    this.getCartoons();
+  }
+
+
+// Función que contiene la segunda parte del fetch, donde se actualiza
+// el estado (array). Es llamada en componentDidMount
+  getCartoons() {
+    GetEpisodes()
+        .then(data => {
+
+          this.setState({
+            AllCharacters : data.results
+          });
+        });
+  }
+
+
+  render() {
+    return (
+        <div className="App">
+          <h1>Hola</h1>
+
+        </div>
+    );
+  };
 }
 
 export default App;
+
