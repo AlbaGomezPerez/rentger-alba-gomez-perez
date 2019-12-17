a1bf0ab489b359cb759210e8c60f5c75b68e37a8
Alba Gómez Pérez <albagope@gmail.com>
2019-12-17 12:17:10 +0100

    add card structure

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -5,12 +5,21 @@ import PropTypes from 'prop-types';
 const EpisodeCard = props => {
     const {AllEpisodes} = props;
     return (
-        <div>
+        <div className="row">
             {AllEpisodes.map((item, index) => {
                     return (
-                        <div key={index}>
-                            <p>{item.name}</p>
-                            <p>{item.air_date}</p>
+                        <div className="col s12 m6" key={index}>
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="images/sample-1.jpg"></img>
+                                        <span className="card-title">{item.name}</span>
+                                        <a className="btn-floating halfway-fab waves-effect waves-light red"><i
+                                            className="material-icons">add</i></a>
+                                </div>
+                                <div className="card-content">
+                                    <p>I am a very simple card.</p>
+                                </div>
+                            </div>
                         </div>
                     )
                 }
