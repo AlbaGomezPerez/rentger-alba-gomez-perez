8e59e0cb3f3ddf6e4be8bc113841a7e7218a85d7
Alba Gómez Pérez <albagope@gmail.com>
2019-12-20 14:57:06 +0100

    Updates

---
+++ precache-manifest.2f98867b7ef9a9e27f8a83a72cf21b1a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e956434b21e7e5fc68a5c8c2f35cfb62",
+    "url": "/rentger-alba-gomez-perez/index.html"
+  },
+  {
+    "revision": "6b2d1d9b1be1af8fe52c",
+    "url": "/rentger-alba-gomez-perez/static/css/main.a2b37f3c.chunk.css"
+  },
+  {
+    "revision": "84a64a5e14fad027720a",
+    "url": "/rentger-alba-gomez-perez/static/js/2.8e3fe3c3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/rentger-alba-gomez-perez/static/js/2.8e3fe3c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b2d1d9b1be1af8fe52c",
+    "url": "/rentger-alba-gomez-perez/static/js/main.3f736aa1.chunk.js"
+  },
+  {
+    "revision": "73b44eac87e16c5c719b",
+    "url": "/rentger-alba-gomez-perez/static/js/runtime-main.e2292c24.js"
+  },
+  {
+    "revision": "f8d000aff8f3f408c4724b15384154f8",
+    "url": "/rentger-alba-gomez-perez/static/media/green.f8d000af.png"
+  },
+  {
+    "revision": "d30b6e6bedf02a2359b3144c3cfa9d2d",
+    "url": "/rentger-alba-gomez-perez/static/media/logo.d30b6e6b.png"
+  },
+  {
+    "revision": "0468fdd37dd6720aa96f6716d976df80",
+    "url": "/rentger-alba-gomez-perez/static/media/rick.0468fdd3.jpg"
+  }
+]);
